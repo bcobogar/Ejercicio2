e7b913011991a1d572f47726104c284a8b200db8
bcobogar <bcobogar@emeal.nttdata.com>
2026-04-21 16:23:24 +0200

    Primer commit

---
+++ Ejercicio2/Ejercicio2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio2App.swift
+//  Ejercicio2
+//
+//  Created by Beatriz Maria Cobo Garcia on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
